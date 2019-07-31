16d7a07867227e02fb5453328bb688dde859083f
Arthur Sommer <art12354@gmail.com>
2019-07-31 15:50:04 -0500

    still working on transitions

--- js/scrips.js
+++ js/scrips.js
@@ -19,6 +19,14 @@ app.controller("HeaderController", function($scope) {
   $scope.nav = false;
   $scope.showNav = function() {
     $scope.nav = !$scope.nav;
+    if ($scope.nav == true) {
+      document.getElementById("button").classList.remove("fa-bars");
+      document.getElementById("button").classList.add("fa-times");
+    }
+    else {
+      document.getElementById("button").classList.remove("fa-times");
+      document.getElementById("button").classList.add("fa-bars");
+    }
   };
   $scope.activeLink = function() {
     // TODO when we have all the pages, have onclick actions that call this function to remap who has the active class
