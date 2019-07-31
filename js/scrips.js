206915d0172fa1534253714004c89b95b0a12ea3
Arthur Sommer <art12354@gmail.com>
2019-07-31 12:32:58 -0500

    initial routing started

--- js/scrips.js
+++ js/scrips.js
@@ -1,3 +1,18 @@
 // angularjs
-var app = angular.module("body", []);
+var app = angular.module("body", ["ngRoute"]);
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/red", {
+    templateUrl : "html/views/red.html"
+  })
+  .when("/green", {
+    templateUrl : "html/views/green.html"
+  })
+  .when("/blue", {
+    templateUrl : "html/views/blue.html"
+  })
+  .otherwise({
+    templateUrl : "html/views/home.html"
+  });
+});
 console.log("hello world!");
