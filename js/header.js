ba124980d3a3abb7e65a20e260793b4e241c9614
Arthur Sommer <art12354@gmail.com>
2019-08-05 18:14:57 -0500

    updated header js 7

--- js/header.js
+++ js/header.js
@@ -1,5 +1,12 @@
 // HEADER
 app.controller("HeaderController", function($scope, $window) {
+  if ($window.innerWidth > 992) {
+    $scope.nav = true;
+    $scope.button = false;
+  } else {
+    $scope.nav = false;
+    $scope.button = true;
+  }
   angular.element($window).bind('resize', function() {
     console.log("Hello world again!");
     if ($window.innerWidth > 992) {
@@ -8,18 +15,20 @@ app.controller("HeaderController", function($scope, $window) {
     } else {
       $scope.nav = false;
       $scope.button = true;
-      $scope.showNav = function() {
-        $scope.nav = !$scope.nav;
-        if ($scope.nav == true) {
-          document.getElementById("button").classList.remove("fa-bars");
-          document.getElementById("button").classList.add("fa-times");
-        } else {
-          document.getElementById("button").classList.remove("fa-times");
-          document.getElementById("button").classList.add("fa-bars");
-        }
-      };
     }
   });
+  $scope.showNav = function() {
+    if ($window.innerWidth < 922) {
+      $scope.nav = !$scope.nav;
+      if ($scope.nav == true) {
+        document.getElementById("button").classList.remove("fa-bars");
+        document.getElementById("button").classList.add("fa-times");
+      } else {
+        document.getElementById("button").classList.remove("fa-times");
+        document.getElementById("button").classList.add("fa-bars");
+      }
+    }
+  };
   $scope.activeLink = function() {
     // TODO when we have all the pages, have onclick actions that call this function to remap who has the active class
   };
